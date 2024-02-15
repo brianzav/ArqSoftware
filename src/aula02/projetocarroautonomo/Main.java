797b949f1379b9c1f2923a3355a4813047a462ef
brian.bzculque <brianzav23@gmail.com>
2024-02-14 21:30:14 -0300

    adding new classes

---
+++ src/aula02/projetocarroautonomo/Main.java
@@ -0,0 +1,7 @@
+package aula02.projetocarroautonomo;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/aula02/projetocarroautonomo/carroEletricoAutonomo/Tesla.java
@@ -0,0 +1,35 @@
+package aula02.projetocarroautonomo.carroEletricoAutonomo;
+
+public class Tesla implements CarroEletricoAutonomo{
+    private int carga;
+    private String partida;
+    private String destino;
+    private boolean modoAutonomo;
+
+    public Tesla(){
+    }
+    @Override
+    public void carregarBateria() {
+
+    }
+
+    @Override
+    public void monitorarBateria() {
+
+    }
+
+    @Override
+    public void definirRota(String partida, String destino) {
+
+    }
+
+    @Override
+    public void ligarModoAutonomo() {
+
+    }
+
+    @Override
+    public void desligarModoAutonomo() {
+
+    }
+}
